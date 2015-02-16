68160d8dbce1b20edf081e83706ccc17ce3f430d
datagutten <datagutten@datagutten.net>
2015-02-16 20:19:01 +0100

    Moved channel list to separate file

---
+++ channellist.php
@@ -0,0 +1,28 @@
+<?php
+$channels=array("animalplanet.discovery.no","hd.animalplanet.discovery.no","science.discovery.no","discovery.no","hd.discovery.no","xd.disneychannel.no","disneychannel.no","nrk1.nrk.no","nrk2.nrk.no","nrk3.nrk.no","nrk3.nrk.no","nrksuper.nrk.no","tv2.no","tvnorge.no","viasat4.no");
+$channellist['Discovery']='discovery.no';
+$channellist['Discovery (N)']='discovery.no';
+$channellist['Discovery HD']='hd.discovery.no';
+$channellist['Discov Science']='science.discovery.no';
+$channellist['Animal Planet']='animalplanet.discovery.no';
+$channellist['Animal Planet HD']='hd.animalplanet.discovery.no';
+
+$channellist['NRK1 HD']='nrk1.nrk.no';
+$channellist['NRK2 HD']='nrk2.nrk.no';
+$channellist['NRK2']='nrk2.nrk.no';
+$channellist['NRK Super _ NRK3']='nrk3super.nrk.no';
+$channellist['NRK Super _ NRK3 HD']='nrk3super.nrk.no';
+$channellist['Disney Channel']='disneychannel.no';
+$channellist['Disney XD']='xd.disneychannel.no';
+$channellist['TV Norge']='tvnorge.no';
+$channellist['TV Norge HD']='tvnorge.no';
+$channellist['TV 2 HD']='tv2.no';
+$channellist['TV 2 (N)']='tv2.no';
+$channellist['Nat Geo Channel']='natgeo.no'; 
+$channellist['Nat Geo HD']='natgeo.no'; 
+$channellist['FOX']='fox.no';
+$channellist['MAX HD']='max.no';
+$channellist['Cartoon Network']='cartoonnetwork.no';
+$channellist['Boomerang']='boomerangtv.no';
+$channellist['Viasat 4 HD']='viasat4.no';
+?>
--- tvguide.class.php
+++ tvguide.class.php
@@ -33,31 +33,7 @@ class tvguide extends filepath
 	}
 	 public function selectchannel($channelstring)
 	 {
-		$channels=array("animalplanet.discovery.no","hd.animalplanet.discovery.no","science.discovery.no","discovery.no","hd.discovery.no","xd.disneychannel.no","disneychannel.no","nrk1.nrk.no","nrk2.nrk.no","nrk3.nrk.no","nrk3.nrk.no","nrksuper.nrk.no","tv2.no","tvnorge.no");
-		$channellist['Discovery']='discovery.no';
-		$channellist['Discovery (N)']='discovery.no';
-		$channellist['Discovery HD']='hd.discovery.no';
-		$channellist['Discov Science']='science.discovery.no';
-		$channellist['Animal Planet']='animalplanet.discovery.no';
-		$channellist['Animal Planet HD']='hd.animalplanet.discovery.no';
-	
-		$channellist['NRK1 HD']='nrk1.nrk.no';
-		$channellist['NRK2 HD']='nrk2.nrk.no';
-		$channellist['NRK2']='nrk2.nrk.no';
-		$channellist['NRK Super _ NRK3']='nrk3super.nrk.no';
-		$channellist['NRK Super _ NRK3 HD']='nrk3super.nrk.no';
-		$channellist['Disney Channel']='disneychannel.no';
-		$channellist['Disney XD']='xd.disneychannel.no';
-		$channellist['TV Norge']='tvnorge.no';
-		$channellist['TV Norge HD']='tvnorge.no';
-		$channellist['TV 2 HD']='tv2.no';
-		$channellist['TV 2 (N)']='tv2.no';
-		$channellist['Nat Geo Channel']='natgeo.no'; 
-		$channellist['Nat Geo HD']='natgeo.no'; 
-		$channellist['FOX']='fox.no';
-		$channellist['MAX HD']='max.no';
-		$channellist['Cartoon Network']='cartoonnetwork.no';
-		$channellist['Boomerang']='boomerangtv.no';
+		require 'channellist.php';
 		if(isset($channellist[$channelstring]))
 			$return=$channellist[$channelstring];
 		else
