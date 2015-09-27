3089d70d5cb9e21a81a39f08b927e36011d46f22
datagutten <datagutten@datagutten.net>
2015-09-27 17:03:29 +0200

    Updated channel list

--- channellist.php
+++ channellist.php
@@ -1,9 +1,8 @@
 <?php
-$channels=array("animalplanet.discovery.no","hd.animalplanet.discovery.no","science.discovery.no","discovery.no","hd.discovery.no","xd.disneychannel.no","disneychannel.no","nrk1.nrk.no","nrk2.nrk.no","nrk3.nrk.no","nrk3.nrk.no","nrksuper.nrk.no","tv2.no","tvnorge.no","viasat4.no");
 $channellist['Discovery']='discovery.no';
 $channellist['Discovery (N)']='discovery.no';
 $channellist['Discovery HD']='hd.discovery.no';
-$channellist['Discov Science']='science.discovery.no';
+$channellist['Discovery Science']='science.discovery.no';
 $channellist['Animal Planet']='animalplanet.discovery.no';
 $channellist['Animal Planet HD']='hd.animalplanet.discovery.no';
 
@@ -14,15 +13,22 @@ $channellist['NRK Super _ NRK3']='nrk3super.nrk.no';
 $channellist['NRK Super _ NRK3 HD']='nrk3super.nrk.no';
 $channellist['Disney Channel']='disneychannel.no';
 $channellist['Disney XD']='xd.disneychannel.no';
+//SBS Disovery
 $channellist['TV Norge']='tvnorge.no';
 $channellist['TV Norge HD']='tvnorge.no';
-$channellist['TV 2 HD']='tv2.no';
+$channellist['MAX HD']='max.no';
+
+$channellist['TV2 HD']='tv2.no';
 $channellist['TV 2 (N)']='tv2.no';
 $channellist['Nat Geo Channel']='natgeo.no'; 
 $channellist['Nat Geo HD']='natgeo.no'; 
+$channellist['Nat Geo HD (N)']='natgeo.no';
 $channellist['FOX']='fox.no';
-$channellist['MAX HD']='max.no';
+
 $channellist['Cartoon Network']='cartoonnetwork.no';
 $channellist['Boomerang']='boomerangtv.no';
 $channellist['Viasat 4 HD']='viasat4.no';
+$channellist['TV3 HD']='tv3.no';
+
+$channels=array_values($channellist);
 ?>
