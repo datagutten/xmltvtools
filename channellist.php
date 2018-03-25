f1e0c1ea147aacf69128220b50486bd1634cf357
datagutten <datagutten@datagutten.net>
2018-03-25 16:45:44 +0200

    Update channel list

--- channellist.php
+++ channellist.php
@@ -7,12 +7,16 @@ $channellist['Animal Planet']='animalplanet.discovery.no';
 $channellist['Animal Planet HD']='hd.animalplanet.discovery.no';
 
 $channellist['NRK1 HD']='nrk1.nrk.no';
+$channellist['NRK1 Østlandssendingen']='nrk1.nrk.no';
+$channellist['NRK1 Østlandssendingen']='nrk1.nrk.no';
+$channellist['NRK1 Østfold']='nrk1.nrk.no';
 $channellist['NRK2 HD']='nrk2.nrk.no';
 $channellist['NRK2']='nrk2.nrk.no';
 $channellist['NRK Super _ NRK3']='nrk3super.nrk.no';
 $channellist['NRK Super _ NRK3 HD']='nrk3super.nrk.no';
 $channellist['Disney Channel']='disneychannel.no';
 $channellist['Disney XD']='xd.disneychannel.no';
+$channellist['Disney XD (N)']='xd.disneychannel.no';
 //SBS Disovery
 $channellist['TV Norge']='tvnorge.no';
 $channellist['TV Norge HD']='tvnorge.no';
@@ -20,12 +24,14 @@ $channellist['MAX HD']='max.no';
 $channellist['VOX']='voxtv.no';
 $channellist['VOX HD']='voxtv.no';
 $channellist['FEM']='fem.no';
+$channellist['FEM HD']='fem.no';
 $channellist['Discovery HD (N)']='discovery.no';
 $channellist['Discovery HD Showcase']='hd.discovery.no';
 
 $channellist['TV2 HD']='tv2.no';
-
 $channellist['TV 2 (N)']='tv2.no';
+$channellist['TV2 Zebra HD']='zebra.tv2.no';
+
 $channellist['Nat Geo Channel']='natgeo.no'; 
 $channellist['Nat Geo HD']='natgeo.no'; 
 $channellist['Nat Geo HD (N)']='natgeo.no';
@@ -37,9 +43,9 @@ $channellist['Viasat 4 HD']='viasat4.no';
 $channellist['TV3 HD']='tv3.no';
 $channellist['TV3']='tv3.no';
 
-$channels['boomerangtv.no']='Boomerang';
 $channellist['FOX HD']='foxtv.no';
 
+$channellist['BBC Earth HD']='hd.bbcknowledge.no';
 
 $channels=array_values($channellist);
 ?>
