658c1d2c76d850c3cda47ecd25f90712a82950a8
datagutten <datagutten@datagutten.net>
2019-10-04 21:15:21 +0200

    Rename test to match class name change

--- tests/common/filenameTest.php
+++ tests/common/filenameTest.php
@@ -2,26 +2,26 @@
 
 namespace datagutten\xmltv\tests\tools\common;
 
-use datagutten\xmltv\tools\common\file;
+use datagutten\xmltv\tools\common\filename;
 use PHPUnit\Framework\TestCase;
 
-class fileTest extends TestCase
+class filenameTest extends TestCase
 {
     public function testFile_path()
     {
-        $path = file::file_path('test.no', 'xmltv', 1570125820, 'xml');
+        $path = filename::file_path('test.no', 'xmltv', 1570125820, 'xml');
         $this->assertEquals('test.no/xmltv/2019/test.no_2019-10-03.xml', $path);
     }
 
     public function testFolder()
     {
-        $folder = file::folder('test.no', 'xmltv', 1570125820);
+        $folder = filename::folder('test.no', 'xmltv', 1570125820);
         $this->assertEquals('test.no/xmltv/2019', $folder);
     }
 
     public function testFilename()
     {
-        $file_name = file::filename('test.no', 1570125820, 'xml');
+        $file_name = filename::filename('test.no', 1570125820, 'xml');
         $this->assertEquals('test.no_2019-10-03.xml', $file_name);
     }
 }
