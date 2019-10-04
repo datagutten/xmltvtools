4d85f31232e299ac59e750e6143d21406bf09d7e
datagutten <datagutten@datagutten.net>
2019-10-04 20:57:04 +0200

    Error handling and PHPdoc

--- xmltv/common/files.php
+++ xmltv/common/files.php
@@ -5,18 +5,28 @@ namespace datagutten\xmltv\tools\common;
 
 
 use Exception;
+use FileNotFoundException;
 use Symfony\Component\Filesystem\Filesystem;
 
 class files
 {
     public $xmltv_path;
     public $filesystem;
+
+    /**
+     * files constructor.
+     * @throws Exception
+     * @throws FileNotFoundException
+     */
     function __construct()
     {
         $config = require 'config.php';
         if(empty($config['xmltv_path']))
             throw new Exception('xmltv_path not set in config');
         $this->xmltv_path = $config['xmltv_path'];
+        if(!file_exists($this->xmltv_path))
+            throw new FileNotFoundException($this->xmltv_path);
+
         $this->filesystem = new Filesystem();
     }
 
@@ -30,5 +40,4 @@ class files
         else
             return $file;
     }
-
 }
