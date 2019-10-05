fd78f8c1fd0c67d09ddb02d770f0e10a9bcb55e2
datagutten <datagutten@datagutten.net>
2019-10-05 09:23:46 +0200

    Add more default argument values

--- xmltv/common/files.php
+++ xmltv/common/files.php
@@ -11,8 +11,11 @@ use Symfony\Component\Filesystem\Filesystem;
 class files
 {
     public $xmltv_path;
+    public $default_sub_folder;
+    /**
+     * @var Filesystem
+     */
     public $filesystem;
-
     /**
      * files constructor.
      * @throws Exception
@@ -24,14 +27,24 @@ class files
         if(empty($config['xmltv_path']))
             throw new Exception('xmltv_path not set in config');
         $this->xmltv_path = $config['xmltv_path'];
+
         if(!file_exists($this->xmltv_path))
             throw new FileNotFoundException($this->xmltv_path);
+
+        if(empty($config['xmltv_default_sub_folder']))
+            throw new Exception('xmltv_default_sub_folder not set in config');
+        $this->default_sub_folder = $config['xmltv_default_sub_folder'];
 
         $this->filesystem = new Filesystem();
     }
 
     function file($channel,$timestamp = null,$sub_folder = null, $extension = 'xml')
     {
+        if(empty($timestamp))
+            $timestamp=strtotime('midnight');
+        if(empty($sub_folder))
+            $sub_folder = $this->default_sub_folder;
+
         $folder = $this->xmltv_path.'/'.filename::folder($channel, $sub_folder, $timestamp);
         $this->filesystem->mkdir($folder);
         $file = $folder.'/'.filename::filename($channel, $timestamp, $extension);
