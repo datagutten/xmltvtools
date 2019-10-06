6e8b5bc5425bfeccb4e452f5a67e0cb9b0ba5670
datagutten <datagutten@datagutten.net>
2019-10-06 11:21:23 +0200

    Add more tests for parser

--- tests/xmltv/parse/parserTest.php
+++ tests/xmltv/parse/parserTest.php
@@ -16,10 +16,49 @@ use PHPUnit\Framework\TestCase;
 class parserTest extends TestCase
 {
     /**
-     * @throws FileNotFoundException
-     * @throws InvalidXMLFileException
+     * @var parser
      */
-    function testCombine()
+    public $parser;
+    public function setUp(): void
+    {
+        $config = file_get_contents(__DIR__.'/test_config.php');
+        $config = str_replace('__DIR__', __DIR__, $config);
+        file_put_contents(__DIR__.'/config.php', $config);
+        set_include_path(__DIR__);
+        $this->parser = new parser();
+    }
+    public function tearDown(): void
+    {
+        unlink(__DIR__.'/config.php');
+    }
+
+    public function testFind_program()
+    {
+        $program = $this->parser->find_program(strtotime('2019-10-03 22:55'), 'natgeo.no');
+        $this->assertEquals('Vinterveiens helter', $program->{'title'});
+        $this->assertEquals('20191003230000 +0000', $program->attributes()->{'start'});
+    }
+
+    public function testGet_programs()
+    {
+        $programs = $this->parser->get_programs('natgeo.no', strtotime('2019-10-04'));
+        $this->assertIsArray($programs);
+        $this->assertEquals('20191004000000 +0000', $programs[0]->attributes()->{'start'});
+    }
+
+    public function testGet_programsNotCombined()
+    {
+        $programs = $this->parser->get_programs('natgeo.no', strtotime('2019-10-04'), false);
+        $this->assertIsArray($programs);
+        $this->assertEquals('20191004060000 +0000', $programs[0]->attributes()->{'start'});
+    }
+
+    public function testSeason_episode()
+    {
+
+    }
+
+    public function testCombine_days()
     {
         $parser = new parser();
         $day1 = $parser->files->load_file('natgeo.no', strtotime('2019-10-03'));
