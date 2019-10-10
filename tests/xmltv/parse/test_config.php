fe73e32b5663ad5ed9d4216b486fa671708a2324
datagutten <datagutten@datagutten.net>
2019-10-10 19:35:08 +0200

    Commit test config file

---
+++ tests/xmltv/parse/test_config.php
@@ -0,0 +1,6 @@
+<?php
+$config['xmltv_path'] = '__DIR__/test_data';
+$config['xmltv_default_sub_folder'] = 'xmltv_php';  //Sub folder in the channel folder
+$config['xmltv_alternate_sub_folders'] = array('xmltv');
+
+return $config;
