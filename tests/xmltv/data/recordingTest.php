7e7d20fa0bea30e480d4f6de6995a742c01cd624
datagutten <datagutten@datagutten.net>
2020-08-23 21:35:15 +0200

    Add class for recording info

---
+++ tests/xmltv/data/recordingTest.php
@@ -0,0 +1,86 @@
+<?php /** @noinspection PhpUnhandledExceptionInspection */
+
+namespace datagutten\xmltv\tests\tools\xmltv\data;
+
+
+use datagutten\tools\files\files;
+use datagutten\xmltv\tools\data\program;
+use datagutten\xmltv\tools\data\recording;
+use datagutten\xmltv\tools\exceptions\InvalidFileNameException;
+use FileNotFoundException;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+date_default_timezone_set('Europe/Oslo');
+class recordingTest extends TestCase
+{
+    public function testFileNotFound()
+    {
+        $this->expectException(FileNotFoundException::class);
+        new recording('z:/opptak/Phineas and Ferb/Phineas og Ferb S01E29 - Boyfriend from 27,000 B.C.ts');
+    }
+
+    public function testInvalidFileName()
+    {
+        $test_file = files::path_join(__DIR__, '..', 'test_data', 'Reklame Kornmo Treider 41.mp4' );
+        $this->expectException(InvalidFileNameException::class);
+        new recording($test_file);
+    }
+
+    public function testPrograms()
+    {
+        $test_file = files::path_join(__DIR__, '..', 'test_data', '20200605 0855 - Disney XD (N) - Phineas og Ferb x4.ts' );
+        $xmltv_path = files::path_join(__DIR__, '..', 'parse', 'test_data');
+        $recording = new recording($test_file, $xmltv_path);
+        $programs = $recording->programs();
+        $this->assertIsArray($programs);
+    }
+    public function testNearest()
+    {
+        $test_file = files::path_join(__DIR__, '..', 'test_data', '20200605 0855 - Disney XD (N) - Phineas og Ferb x4.ts' );
+        $xmltv_path = files::path_join(__DIR__, '..', 'parse', 'test_data');
+        $recording = new recording($test_file, $xmltv_path);
+        $program = $recording->program_nearest();
+        $this->assertSame(1591340400, $program->start_timestamp);
+        $this->assertSame('Phineas og Ferb', $program->title);
+    }
+
+    public function testEitInfo()
+    {
+        $test_file = files::path_join(__DIR__, '..', 'test_data', '20200605 0855 - Disney XD (N) - Phineas og Ferb x4.ts' );
+        $recording = new recording($test_file);
+        $eit = $recording->eit_info();
+        $this->assertInstanceOf(program::class, $eit);
+    }
+
+    public function testNoXMLTV()
+    {
+        $test_file = files::path_join(__DIR__, '..', 'test_data', '20200605 0855 - Disney XD (N) - Phineas og Ferb x4.ts' );
+        $this->expectException(InvalidArgumentException::class);
+        $recording = new recording($test_file);
+        $recording->programs();
+    }
+    public function testNoXMLTVNearest()
+    {
+        $test_file = files::path_join(__DIR__, '..', 'test_data', '20200605 0855 - Disney XD (N) - Phineas og Ferb x4.ts' );
+        $this->expectException(InvalidArgumentException::class);
+        $recording = new recording($test_file);
+        $recording->program_nearest();
+    }
+
+    public function testTime()
+    {
+        $test_file = files::path_join(__DIR__, '..', 'test_data', '20200605 0855 - Disney XD (N) - Phineas og Ferb x4.ts' );
+        $recording = new recording($test_file);
+        $time = $recording->time();
+        $this->assertSame($time, '08:55-08:55');
+    }
+
+    public function testEitTime()
+    {
+        $test_file = files::path_join(__DIR__, '..', 'test_data', '20200605 0855 - Disney XD (N) - Phineas og Ferb x4.ts' );
+        $recording = new recording($test_file);
+        $time = $recording->eit_time();
+        $this->assertSame($time, '10:00-10:29');
+    }
+}
---
+++ xmltv/data/recording.php
@@ -0,0 +1,153 @@
+<?php
+
+
+namespace datagutten\xmltv\tools\data;
+
+
+use datagutten\dreambox;
+use datagutten\video_tools\exceptions as video_exceptions;
+use datagutten\xmltv\tools\exceptions as xmltv_exceptions;
+use datagutten\xmltv\tools\exceptions\XMLTVException;
+use datagutten\xmltv\tools\parse;
+use DependencyFailedException;
+use FileNotFoundException;
+use InvalidArgumentException;
+
+class recording extends recording_file
+{
+    /**
+     * @var dreambox\recording_info
+     */
+    public $dreambox;
+
+    /**
+     * @var string Date and time for the recording start
+     */
+    public $start_datetime;
+
+    /**
+     * @var int Recording start timestamp
+     */
+    public $start_timestamp;
+
+    /**
+     * @var int Recording end timestamp
+     */
+    public $end_timestamp;
+
+    /**
+     * @var string Channel name from file name
+     */
+    public $channel_name;
+
+    /**
+     * @var program|array
+     */
+    public $eit;
+
+    /**
+     * @var parse\merger
+     */
+    public $xmltv_parser;
+
+    /**
+     * recording constructor.
+     * @param string $file Recording file
+     * @param string $xmltv_path XMLTV root path
+     * @param array $xmltv_sub_folders Sub folders of each channel to load data from
+     * @throws FileNotFoundException XMLTV path not found
+     * @throws FileNotFoundException
+     * @throws xmltv_exceptions\InvalidFileNameException
+     * @throws XMLTVException
+     */
+    function __construct($file, $xmltv_path = '', $xmltv_sub_folders = ['xmltv'])
+    {
+        parent::__construct($file);
+
+        $this->dreambox = new dreambox\recording_info();
+
+        try {
+            $this->duration = $this->duration();
+        }
+        catch (DependencyFailedException|video_exceptions\DurationNotFoundException $e) {
+            trigger_error('Unable to get duration: '.$e->getMessage());
+        }
+
+        $info = $this->dreambox->parse_file_name($file);
+        $this->start_datetime = $info['datetime']; //Date and time from file name
+        $this->channel_name = $info['channel']; //Channel from file name
+        $this->start_timestamp=strtotime($info['datetime']);
+        $this->end_timestamp = $this->start_timestamp + $this->duration;
+
+        $this->eit = $this->eit_info();
+        if(!empty($xmltv_path))
+            $this->xmltv_parser = new parse\merger($xmltv_path, $xmltv_sub_folders);
+    }
+
+    /**
+     * Get XMLTV channel id
+     * @return string XMLTV channel id
+     * @throws xmltv_exceptions\ChannelNotFoundException Channel not found
+     */
+    function channel_id() {
+        return $this->dreambox->channels->name_to_id($this->channel_name);
+    }
+
+    /**
+     * Get programs in the recording
+     * @return array
+     * @throws xmltv_exceptions\ProgramNotFoundException Program not found
+     * @throws xmltv_exceptions\ChannelNotFoundException Channel not found
+     */
+    function programs()
+    {
+        if(empty($this->xmltv_parser))
+            throw new InvalidArgumentException('XMLTV path not specified');
+
+        $channel = $this->channel_id();
+        $info_array = [];
+        $end_timestamp = $this->start_timestamp;
+
+        while($end_timestamp<$this->start_timestamp+$this->duration) {
+            $program_xml = $this->xmltv_parser->find_program($end_timestamp, $channel);
+            $end_timestamp = strtotime($program_xml->attributes()->{'stop'});
+            $info_array[] = program::from_xmltv($program_xml);
+        }
+        return $info_array;
+    }
+
+    /**
+     * Find the nearest program start in the recording
+     * @return program
+     * @throws xmltv_exceptions\ProgramNotFoundException Program not found
+     * @throws xmltv_exceptions\ChannelNotFoundException Channel not found
+     */
+    function program_nearest()
+    {
+        if(empty($this->xmltv_parser))
+            throw new InvalidArgumentException('XMLTV path not specified');
+
+        $xmltv = $this->xmltv_parser->find_program($this->start_timestamp,$this->channel_id(),'nearest');
+        return program::from_xmltv($xmltv);
+    }
+
+    function time()
+    {
+        return sprintf('%s-%s',date('H:i',$this->start_timestamp),date('H:i',$this->end_timestamp));
+    }
+    function eit_time()
+    {
+        return sprintf('%s-%s',date('H:i',$this->eit->start_timestamp),date('H:i',$this->eit->end_timestamp));
+    }
+
+    /**
+     * Get information from EIT file
+     * @return program
+     * @throws FileNotFoundException EIT file not found
+     */
+    function eit_info()
+    {
+        $eit_file = sprintf('%s/%s.eit', $this->pathinfo['dirname'], $this->pathinfo['filename']);
+        return program::from_eit($eit_file);
+    }
+}
