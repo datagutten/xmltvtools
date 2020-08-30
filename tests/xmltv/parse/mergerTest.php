bcfa0d85b0e1db8959681c9833dab26b22e68f4f
datagutten <datagutten@datagutten.net>
2020-08-30 15:50:33 +0200

    Handle merging parser errors

--- tests/xmltv/parse/mergerTest.php
+++ tests/xmltv/parse/mergerTest.php
@@ -16,6 +16,20 @@ class mergerTest extends TestCase
         $this->assertEquals('3.119.', $program->{'episode-num'});
     }
 
+    public function testFind_program2()
+    {
+        $merger = new merger(__DIR__.'/test_data', ['xmltv', 'xmltv_php']);
+        $program = $merger->find_program(strtotime('2020-06-05 05:00 +00:00'), 'xd.disneychannel.no');
+        $this->assertEquals('Phineas og Ferb', $program->{'title'});
+    }
+
+    public function testFind_programSingleParser()
+    {
+        $merger = new merger(__DIR__.'/test_data', ['xmltv']);
+        $program = $merger->find_program(strtotime('2020-06-05 05:00 +00:00'), 'xd.disneychannel.no');
+        $this->assertEquals('Phineas og Ferb', $program->{'title'});
+    }
+
     public function testInvalidFile()
     {
         $merger = new merger(__DIR__.'/test_data', ['xmltv_php', 'xmltv']);
--- xmltv/parse/merger.php
+++ xmltv/parse/merger.php
@@ -3,9 +3,7 @@
 
 namespace datagutten\xmltv\tools\parse;
 
-use datagutten\xmltv\tools\exceptions\ProgramNotFoundException;
-use FileNotFoundException;
-use SimpleXMLElement;
+use datagutten\xmltv\tools\exceptions\XMLTVException;
 
 /**
  * Merge information from different xmltv files
@@ -33,12 +31,17 @@ class merger extends parser
          */
         foreach ($this->parsers as $parser)
         {
-            $program = $parser->find_program($search_time,$programs_xml_or_channel,$mode);
-            $keys = array_keys((array)$program);
-            $diff = array_diff($keys, $base_keys);
-            foreach($diff as $field)
+            try {
+                $program = $parser->find_program($search_time, $channel, $mode);
+                $keys = array_keys((array)$program);
+                $diff = array_diff($keys, $base_keys);
+                foreach ($diff as $field) {
+                    $base_program->$field = $program->$field;
+                }
+            }
+            catch (XMLTVException $e)
             {
-                $base_program->$field = $program->$field;
+                continue;
             }
         }
         return $base_program;
