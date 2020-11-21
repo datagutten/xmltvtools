datagutten/xmltvtools

commit 3fe4c1918e9f4d454167117b852df075c2c5fa0c

Fix issue when requesting information about a program after the last

xmltv/parse/parser.php
@@ -175,8 +175,13 @@ class parser
                 }
             }
 
-            if($mode=='next' && $time_to_start[$key]>=0) //Find first program which has not started
-                return $program;
+            if($mode=='next')
+            {
+                if($time_to_start[$key]>=0) //Find first program which has not started
+                    return $program;
+                else
+                    throw new ProgramNotFoundException('Nothing on air at given time');
+            }
             elseif($mode=='now')
             {
                 if($time_to_start[$key]>0) //Current program has not started, return the previous (running now)

xmltv/data/Recording.php
@@ -115,13 +115,22 @@ class Recording extends RecordingFile
             throw new InvalidArgumentException('XMLTV path not specified');
 
         $channel = $this->channelId();
-        $info_array = [];
-        $end_timestamp = $this->start_timestamp;
+        $recording_end_timestamp = $this->start_timestamp+$this->duration;
 
-        while($end_timestamp<$this->start_timestamp+$this->duration) {
-            $program_xml = $this->xmltv_parser->find_program($end_timestamp, $channel);
-            $end_timestamp = strtotime($program_xml->attributes()->{'stop'});
+        $first_program_xml = $this->xmltv_parser->find_program($this->start_timestamp, $channel);
+        $info_array = [Program::fromXMLTV($first_program_xml)];
+        $end_timestamp = strtotime($first_program_xml->attributes()->{'stop'});
+
+        while($end_timestamp<$recording_end_timestamp) {
+            try {
+                $program_xml = $this->xmltv_parser->find_program($end_timestamp, $channel, 'next');
+            }
+            catch (xmltv_exceptions\ProgramNotFoundException $e)
+            {
+                break;
+            }
             $info_array[] = Program::fromXMLTV($program_xml);
+            $end_timestamp = strtotime($program_xml->attributes()->{'stop'});
         }
         return $info_array;
     }

commit 67efc3ed49547ff3ad79c1e0c2d6ca0ff80e5d42

Fix fetching of next program Broken in 3fe4c1918e9f4d454167117b852df075c2c5fa0c

xmltv/parse/parser.php
@@ -180,10 +180,10 @@ class parser
 
             if($mode=='next')
             {
-                if($time_to_start[$key]>=0) //Find first program which has not started
-                    return $program;
-                else
+                if(!isset($programs_xml[$key+1]))
                     throw new ProgramNotFoundException('Nothing on air at given time');
+                elseif($time_to_start[$key]>=0) //Find first program which has not started
+                    return $program;
             }
             elseif($mode=='now')
             {