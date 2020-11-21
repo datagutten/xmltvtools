d86c7c2b1bf60e760f7a3ddcf4f9639129578f9e
datagutten <datagutten@datagutten.net>
2020-11-21 13:39:39 +0100

    Remove old config file

--- config_xmltv_sample.php
+++
@@ -1,8 +0,0 @@
-<?Php
-$xmltvpath='http://xmltv.host.local';
-
-$xmltv_subfolder_main='xmltv'; //Subfolder for main source
-$xmltv_subfolder_alt='xmltv2'; //Subfolder for alternative source
-
-$xmltv_cache='/home/xmltv'; //Local cache folder
-?>
