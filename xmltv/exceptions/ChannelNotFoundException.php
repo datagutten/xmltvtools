6ba670894b5434bab34f867ed324d7720bcf6ce0
Anders Birkenes <datagutten@datagutten.net>
2026-03-22 11:18:49 +0100

    Mark arguments as nullable

--- xmltv/exceptions/ChannelNotFoundException.php
+++ xmltv/exceptions/ChannelNotFoundException.php
@@ -8,7 +8,7 @@ use Exception;
 
 class ChannelNotFoundException extends XMLTVException
 {
-    public function __construct($channel, $code = 0, Exception $previous = null) {
+    public function __construct($channel, $code = 0, ?Exception $previous = null) {
         $message = sprintf('Channel not found: %s', $channel);
         parent::__construct($message, $code, $previous);
     }
--- xmltv/exceptions/InvalidFileNameException.php
+++ xmltv/exceptions/InvalidFileNameException.php
@@ -8,7 +8,7 @@ use Exception;
 
 class InvalidFileNameException extends XMLTVException
 {
-	public function __construct($file, $code = 0, Exception $previous = null) {
+	public function __construct($file, $code = 0, ?Exception $previous = null) {
 		$message = sprintf('Invalid file name: %s', $file);
 		parent::__construct($message, $code, $previous);
 	}
