4b03518e3b4a44b552f8e2bc6c89d57b794da251
Anders Birkenes <datagutten@datagutten.net>
2026-03-22 10:13:01 +0100

    Add test for XML output with time zone

--- tests/xmltv/build/xmltv/buildTest.php
+++ tests/xmltv/build/xmltv/buildTest.php
@@ -4,8 +4,11 @@ namespace datagutten\xmltv\tests\tools\xmltv\build\xmltv;
 
 use datagutten\xmltv\tools\build\programme;
 use datagutten\xmltv\tools\build\tv;
+use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 use SimpleXMLElement;
+
+date_default_timezone_set('Europe/Oslo');
 
 class buildTest extends TestCase
 {
@@ -24,5 +27,18 @@ class buildTest extends TestCase
         $programme = new programme(strtotime('2020-01-27 18:00'), $tv);
         $this->assertSame($tv->language, $programme->default_lang);
         $this->assertInstanceOf(SimpleXMLElement::class, $programme->xml);
+        $output = $tv->xml->asXML();
+        $this->assertStringContainsString('start="20200127180000 +0200"', $output);
+    }
+
+    public function testProgrammeDateObj()
+    {
+        $tv = new tv('test.no', 'nb', 'test-grabber');
+        $this->assertEquals('test-grabber', $tv->generator);
+        $programme = new programme(new DateTimeImmutable('2020-01-27 16:00 +00:00'), $tv);
+        $this->assertSame($tv->language, $programme->default_lang);
+        $this->assertInstanceOf(SimpleXMLElement::class, $programme->xml);
+        $output = $tv->xml->asXML();
+        $this->assertStringContainsString('start="20200127160000 +0000"', $output);
     }
 }
